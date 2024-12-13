b1369f9bd423bb6f1117961cd7e81056df98e93c
Bernardo Bressan <bressan.mattos@estudante.ufjf.br>
2024-12-12 22:34:22 -0300

    conserta funcionario

--- src/Funcionarios.tsx
+++ src/Funcionarios.tsx
@@ -11,7 +11,7 @@ export default function Funcionario ({funcionario}: FuncionarioProps)
         <div className="Funcionario">
             <div className="nome_completo">{funcionario.nome_completo}</div>
             <div className="setor">{funcionario.setor}</div>
-            <div className="data_amissao">{funcionario.data_amissao}</div> 
+            <div className="data_amissao">{funcionario.data_admissao}</div> 
             <div className="salario_base">{funcionario.salario_base}</div>
         </div>
     )
